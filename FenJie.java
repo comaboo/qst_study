5c302ee269501dcb3e620f3a7f0d620f1fe0f8f4
comaboo <53267814+comaboo@users.noreply.github.com>
2019-07-24 23:43:19 +0800

    Add files via upload

---
+++ FenJie.java
@@ -0,0 +1,37 @@
+package com.exam;
+
+import java.util.Scanner;
+
+public class FenJie {
+	
+	//找出最小质因数
+	public static int findYS(int i) {
+		int j;
+		for(j=2;j<=i;j++) {
+			if(i%j==0) {
+				break;
+			}
+		}
+		return j;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("请输入一个整数：");
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		int k=findYS(num);
+		//判断是否质因数是本身，不是则迭代找出最小质因数
+		if(k==num) {
+			System.out.println(num+"="+"1*"+k);
+		}else {
+			System.out.print(num+"="+k);
+			while(k!=num){
+				num=num/k;
+				k=findYS(num);
+				System.out.print("*"+k);
+			}
+		 
+	}
+	}
+
+}
